7b0afbdb5e18805b5640fda2dcd1e8956f08179a
Wren Powell <wrenp@duck.com>
2023-05-26 13:03:32 -0400

    Add article details to feed article

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,29 @@ function App() {
             title="Starship Explodes Again"
             subtitle={'"This was the best possible outcome," says Musk.'}
             categories={["News", "Tech"]}
+            date={new Date("2023-04-30T13:05:00Z")}
+            readTimeMinutes={11}
+            authorName="Joyce Messier"
           ></FeedArticle>
           <FeedArticle
             url={new URL("https://example.com")}
             title="Literal Bull Loose in New York Stock Exchange"
             subtitle="The SEC is responding to reports of a bull market."
             categories={["Finance"]}
+            date={new Date("2023-05-02T10:23:11Z")}
+            readTimeMinutes={7}
+            authorName="Kim Kitsuragi"
+          ></FeedArticle>
+          <FeedArticle
+            url={new URL("https://example.com")}
+            title="Local Trans Woman is Very Bad at Sports"
+            subtitle={
+              "\"I'm not allowed to compete on the women's team. Not because I'm trans—I'm just really uncoordinated.\""
+            }
+            categories={["Sports"]}
+            date={new Date("2023-05-03T15:42:13Z")}
+            readTimeMinutes={6}
+            authorName="Klaasje Amandou"
           ></FeedArticle>
         </Feed>
       </main>
--- src/components/FeedArticle.tsx
+++ src/components/FeedArticle.tsx
@@ -1,13 +1,31 @@
 import "./FeedArticle.css";
+import { BsCalendar2, BsClock, BsPersonCircle } from "react-icons/bs";
 
 export interface FeedArticleProps {
   url: URL;
   title: string;
   subtitle: string;
   categories: ReadonlyArray<string>;
+  date: Date;
+  readTimeMinutes: number;
+  authorName: string;
 }
 
-export const FeedArticle = ({ url, title, subtitle, categories }: FeedArticleProps) => {
+const dateFormatOptions = {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+} as const;
+
+export const FeedArticle = ({
+  url,
+  title,
+  subtitle,
+  categories,
+  date,
+  readTimeMinutes,
+  authorName,
+}: FeedArticleProps) => {
   return (
     <article className="FeedArticle">
       <a className="article-body" href={url.toString()}>
@@ -20,6 +38,24 @@ export const FeedArticle = ({ url, title, subtitle, categories }: FeedArticlePro
           <h2 className="article-title">{title}</h2>
           <h3 className="article-subtitle">{subtitle}</h3>
         </div>
+        <footer className="article-detail-list">
+          <div className="article-detail">
+            <BsCalendar2 />
+            <time className="article-detail-text" dateTime={date.toISOString()}>
+              {new Intl.DateTimeFormat(undefined, dateFormatOptions).format(date)}
+            </time>
+          </div>
+          <div className="article-detail">
+            <BsClock />
+            <time className="article-detail-text" dateTime={`PT0H${readTimeMinutes}M`}>
+              {readTimeMinutes} minute read
+            </time>
+          </div>
+          <div className="article-detail">
+            <BsPersonCircle />
+            <span className="article-detail-text">{authorName}</span>
+          </div>
+        </footer>
       </a>
     </article>
   );
