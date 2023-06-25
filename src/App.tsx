cce60eb5ffd5c77a32e90bb38718582b135478bb
Wren Powell <wrenp@duck.com>
2023-06-25 17:10:16 -0400

    Set color scheme based on user preference

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
 import { SyndicationFeed } from "./components/SyndicationFeed";
 
+const setColorScheme = (isDark: boolean) => {
+  const colorScheme = isDark ? "dark" : "light";
+  document.querySelector("html")?.setAttribute("data-scheme", colorScheme);
+};
+
 function App() {
+  useEffect(() => {
+    const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+    setColorScheme(darkModePreference.matches);
+    darkModePreference.addEventListener("change", (e) => setColorScheme(e.matches));
+  }, []);
+
   return (
     <div className="App">
       <main>
