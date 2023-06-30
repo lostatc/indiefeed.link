723ffc26de087cc074e2f907a3e51ba7a0b534b1
Wren Powell <wrenp@duck.com>
2023-06-30 09:32:12 -0400

    Add dynamic tooltip to copy feed URL button

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,8 @@
 import { FeedArticleData } from "./FeedArticle";
 import "./Feed.css";
-import { ReactElement, useCallback, useState } from "react";
+import { ReactElement, useCallback, useEffect, useRef, useState } from "react";
 import { BsClipboard, BsClipboardCheck } from "react-icons/bs";
+import { Tooltip } from "bootstrap";
 
 export const Feed = ({
   title,
@@ -26,6 +27,32 @@ export const Feed = ({
     }, 1000);
   }, [url]);
 
+  const copyButtonRef = useRef<HTMLButtonElement>(null);
+
+  const [tooltip, setTooltip] = useState<Tooltip>();
+
+  useEffect(() => {
+    if (copyButtonRef.current !== null) {
+      setTooltip(new Tooltip(copyButtonRef.current));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (copyButtonRef.current === null || tooltip === undefined) return;
+
+    if (copied) {
+      copyButtonRef.current.setAttribute("data-bs-original-title", "Copied!");
+    } else {
+      copyButtonRef.current.setAttribute("data-bs-original-title", "Copy");
+    }
+
+    if (copied) {
+      tooltip.show();
+    } else {
+      tooltip.hide();
+    }
+  }, [copied, tooltip]);
+
   return (
     <>
       <h1 className="feed-name">{title}</h1>
@@ -34,17 +61,21 @@ export const Feed = ({
           <div>Copy this URL into your feed reader</div>
           <div className="feed-url">
             {url}
-            <button onClick={copyUrl}>
+            <button
+              onClick={copyUrl}
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title="Copy"
+              aria-label="Copy"
+              ref={copyButtonRef}
+            >
               <span hidden={copied}>
-                <BsClipboard title="Copy" />
+                <BsClipboard aria-hidden />
               </span>
               <span hidden={!copied}>
-                <BsClipboardCheck title="Copy" />
+                <BsClipboardCheck aria-hidden />
               </span>
             </button>
-          </div>
-          <div className="copied-text" hidden={!copied} aria-live="polite">
-            Copied!
           </div>
         </section>
       )}
