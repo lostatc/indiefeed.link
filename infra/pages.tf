6ec839ab4094bb34e1d171767e0ed2793836c8c8
Wren Powell <wrenp@duck.com>
2023-09-22 08:42:20 -0400

    Revert "Specify Hugo version in infra config"
    This reverts commit 43b4a85df3d57bb75b17be262afa867f60b229a2.
    
    I forgot this project doesn't even use Hugo. I'm tired.

--- infra/pages.tf
+++ infra/pages.tf
@@ -4,7 +4,7 @@ resource "cloudflare_pages_project" "indiefeed" {
   production_branch = "main"
 
   build_config {
-    build_command   = "npm install && npm run build"
+    build_command = "npm install && npm run build"
     destination_dir = "build"
   }
 
@@ -12,29 +12,14 @@ resource "cloudflare_pages_project" "indiefeed" {
     type = "github"
 
     config {
-      owner                         = "lostatc"
-      repo_name                     = "indiefeed.link"
-      production_branch             = "main"
-      pr_comments_enabled           = true
-      deployments_enabled           = true
+      owner = "lostatc"
+      repo_name = "indiefeed.link"
+      production_branch = "main"
+      pr_comments_enabled = true
+      deployments_enabled = true
       production_deployment_enabled = true
-      preview_deployment_setting    = "custom"
-      preview_branch_includes       = ["dev"]
-    }
-  }
-
-  deployment_configs {
-    production {
-      environment_variables = {
-        HUGO_VERSION = "0.104.0"
-      }
-    }
-
-    preview {
-      environment_variables = {
-        HUGO_BASEURL = "/"
-        HUGO_VERSION = "0.104.0"
-      }
+      preview_deployment_setting = "custom"
+      preview_branch_includes = ["dev"]
     }
   }
 }
